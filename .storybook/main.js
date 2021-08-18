8f27d732bca3d24b7b6c95b4485b82eb01e37c40
monoharada <harada@monosus.co.jp>
2021-08-18 10:21:29 +0900

    first-commit

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@whitespace/storybook-addon-html',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ]
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+// import { GlobalStyles } from 'twin.macro'
+// import { css } from 'linaria'
+import '../styles/styles.css'
+import { AppProps } from 'next/app'
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>
+        Nextjs App with TypeScript, ESlint, Jest, Emotion, Tailwind and Twin
+      </title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    {/* <GlobalStyles  /> */}
+    {/* <Global styles={globalStyles} /> */}
+    <Component {...pageProps} />
+  </>
+)
+
+
+
+export default App
