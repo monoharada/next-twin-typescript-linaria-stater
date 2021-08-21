8363beec49a3a8c329e2588ab5e800d2be9ff1c5
monoharada <harada@monosus.co.jp>
2021-08-21 18:18:19 +0900

    test: 💍 create other page
    cssファイルの出力状況調査
    
    ✅ Closes: 2

---
+++ src/pages/other.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { css } from "linaria";
+import tw from "twin.macro";
+import { Button,Text } from "../components";
+
+export default function other() {
+  return (
+    <div className="App">
+      <h1>Next.js + twin.macro + Linaria</h1>
+      <Text>hogehoge</Text>
+      <div
+        className={css`
+          ${tw`mt-[50px] md:bg-gray-500 sm:bg-pink-500 border border-solid border-purple-900 p-6 font-bold`}
+        `}
+      >
+        I am styled with twin.macro + Linaria
+        <Button
+          type="button"
+          isSecondary
+          className={css`
+            ${tw`ml-11`}
+          `}
+        >
+          hoge
+        </Button>
+      </div>
+    </div>
+  );
+}
