32fad9f700281c95d4ba2ad6af825bace6c21d85
monoharada <harada@monosus.co.jp>
2021-08-23 02:31:23 +0900

    ci: eslintを更新
    a11tyとnext/imageの不使用を警告なしに

--- src/components/elements/Text/Text.tsx
+++
@@ -1,7 +0,0 @@
-// import { css } from 'linaria';
-// import tw, { styled, theme } from 'twin.macro';
-// ______________________________________________________
-//
-export const Text: React.FC = () => {
-  return <div></div>;
-};
