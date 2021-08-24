95e56098e2864c3cac765306adae9a0120c87572
monoharada <harada@monosus.co.jp>
2021-08-24 18:11:03 +0900

    refactor: 不要ファイルの削除

--- src/components/elements/Text/Text.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { Text } from './';
-// ______________________________________________________
-//
-storiesOf('elements/Text', module).add('default', () => <Text />);
--- src/components/elements/Text/index.tsx
+++
@@ -1,7 +0,0 @@
-// import { css } from 'linaria';
-// import tw, { styled, theme } from 'twin.macro';
-// ______________________________________________________
-//
-export const Text: React.FC = ({ children }) => {
-  return <div>{children}</div>;
-};
