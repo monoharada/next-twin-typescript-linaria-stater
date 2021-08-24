468d909ef6f622c2b6a978a255f4bcf535bcb1ac
monoharada <harada@monosus.co.jp>
2021-08-24 21:17:38 +0900

    fix: storybookの調整
    webpackにパスのエイリアス追加、静的ファイルをoutディレクトリから読み込み
    
    Closes 3,6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -19,5 +21,26 @@ module.exports = {
   ],
   core: {
     builder: 'webpack5',
+  },
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/elements': path.resolve(__dirname, '../src/components/elements'),
+      '@/modules': path.resolve(__dirname, '../src/components/modules'),
+      '@/compositions': path.resolve(
+        __dirname,
+        '../src/components/compositions',
+      ),
+      '@/lib': path.resolve(__dirname, '../src/lib'),
+      '@/images': path.resolve(__dirname, '../public/images'),
+    }
+
+    // Return the altered config
+    return config
   },
 }
