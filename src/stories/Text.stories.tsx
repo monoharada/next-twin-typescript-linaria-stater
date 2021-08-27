bd2c0469030d8bf870e9f40b3083106d23945c49
mono-tmt <tomita@monosus.co.jp>
2021-08-27 20:02:27 +0900

    style: storiesの初期カタログを作成

--- src/stories/Text.stories.tsx
+++ src/stories/Text.stories.tsx
@@ -7,7 +7,38 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: 'There are a set of rules to keep in mind:',
+        component: 'There are a set of rules to keep in mind: \n kookni',
+      },
+    },
+  },
+  argTypes: {
+    variants: {
+      name: 'variant',
+      description: 'variant descripton',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: false },
+      },
+      control: 'boolean',
+    },
+    children: {
+      name: 'children',
+      description: 'children description',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'text something here' },
+      },
+    },
+    type: {
+      name: 'type',
+      description: 'type description',
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large'],
+      },
+      table: {
+        type: { summary: 'select' },
+        defaultValue: { summary: 'medium' },
       },
     },
   },
@@ -15,3 +46,6 @@ export default {
 
 const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />
 export const Primary = Template.bind({})
+Primary.args = {
+  children: 'text something here',
+}
